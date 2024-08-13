17a74720746f82c038fb8e1cfc36aada8d476d9d
Chris-Loza <christianloza99@gmail.com>
2024-08-13 14:14:41 -0700

    added follower component

---
+++ src/Components/FollowersAndFollowing/Follower.jsx
@@ -0,0 +1,26 @@
+import { Avatar, Box, Flex, VStack } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
+
+const Follower = ({ user }) => {
+  return (
+    <Flex justifyContent={"space-between"} alignItems={"center"} w={"full"}>
+      <Flex alignItems={"center"} gap={2}>
+        <Link to={`/${user.username}`}>
+          <Avatar src={user.profilePicURL} size={"md"} />
+        </Link>
+        <VStack spacing={2} alignItems={"flex-start"}>
+          <Link to={`/${user.username}`}>
+            <Box fontSize={12} fontWeight={"bold"}>
+                {user.fullName}
+            </Box>
+          </Link>
+          <Box fontSize={11} color={"gray.500"}>
+            {user.followers.length} followers
+          </Box>
+        </VStack>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Follower;
