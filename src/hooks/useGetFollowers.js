5ab4cbe1cace4ed1be33171aa62a504211b93dbd
Chris-Loza <christianloza99@gmail.com>
2024-08-13 14:06:01 -0700

    added functionality to useGetFollowers hook(untested)

--- src/hooks/useGetFollowers.js
+++ src/hooks/useGetFollowers.js
@@ -1,5 +1,41 @@
+import { useState } from "react";
+import useUserProfileStore from "../store/userProfileStore";
+import useShowToast from "./useShowToast";
+import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { firestore } from "../firebase/firebase";
+
 const useGetFollowers = () => {
-  return <div>useGetFollowers</div>;
+  const [isLoading, setIsLoading] = useState(true);
+  const [followersList, setFollowersList] = useState([]);
+  const { userProfile } = useUserProfileStore();
+  const showToast = useShowToast();
+
+  const getFollowers = async () => {
+    setIsLoading(true);
+    try {
+      const usersRef = collection(firestore, "users");
+      const q = query(
+        usersRef,
+        where("uid", "in", [userProfile.uid, ...userProfile.followers]),
+        orderBy("uid")
+      );
+
+      const querySnapshot = await getDocs(q);
+      const followers = [];
+      querySnapshot.forEach((doc) => {
+        followers.push({ ...doc.data(), id: doc.id });
+      });
+
+      setFollowersList(followers);
+    } catch (error) {
+      showToast("Error", error.message, "error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  getFollowers();
+  return { isLoading, followersList };
 };
 
 export default useGetFollowers;
